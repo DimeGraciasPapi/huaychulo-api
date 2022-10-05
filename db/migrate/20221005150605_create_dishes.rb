18773f5cb444e465ccafca40b97789bfbe155ac7
emachipana <emachipana0102@gmail.com>
2022-10-05 10:08:40 -0500

    ADD: create dish model

---
+++ db/migrate/20221005150605_create_dishes.rb
@@ -0,0 +1,15 @@
+class CreateDishes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dishes do |t|
+      t.references :category, null: false, foreign_key: true
+      t.string :name
+      t.float :price
+      t.integer :quantity
+      t.text :description
+      t.integer :waiting_time
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_145427) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_150605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_145427) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "dishes", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name"
+    t.float "price"
+    t.integer "quantity"
+    t.text "description"
+    t.integer "waiting_time"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_dishes_on_category_id"
+  end
+
+  add_foreign_key "dishes", "categories"
 end
