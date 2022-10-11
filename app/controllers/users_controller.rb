61eeef9026956da29594e08b44b1b2d87dc3626c
emachipana <emachipana0102@gmail.com>
2022-10-10 23:45:24 -0500

    ADD: users controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: %i[ create ]
+  skip_before_action :validate_admin_user, only: %i[ show update ]
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user.as_json(except: :password_digest), status: :created
+    else
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: current_user.as_json(except: :password_digest)
+  end
+
+  def update
+    if current_user.update(user_params)
+      render json: current_user.as_json(except: :password_digest)
+    else
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    current_user.destroy
+
+    render json: { success: "El usuario fue eliminado" }
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_type, :name, :last_name, :document, :password, :intake, :document_type)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  # sessions
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  post "/signup" => "users#create"
 
+  # profile
+
+  get "/profile" => "users#show"
+  patch "/profile" => "users#update"
+  delete "/profile" => "users#destroy"
+
+  # categories
   resources :categories, except: %i[ show new edit ]
 
+  # dishes
   resources :dishes, except: %i[ new edit ]
 end
