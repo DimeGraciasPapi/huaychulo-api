8e67ee6c9afe1cfab894b321f7959ad2054d10fd
emachipana <emachipana0102@gmail.com>
2022-10-24 12:00:35 -0500

    ADD: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+adminUser = User.new(
+    user_type: 1, 
+    name: "Julio",
+    last_name: "Narvaez Pardabe",
+    document: 20136424867,
+    password: "123456",
+    document_type: 1
+  );
+
+if adminUser.save
+  puts "El usuario administrador fue creado"
+else
+  puts adminUser.errors.full_messages
+end
