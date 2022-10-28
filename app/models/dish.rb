c42d79260f79040e5bebd327a085f7495c3d8aaf
emachipana <emachipana0102@gmail.com>
2022-10-27 19:30:24 -0500

    FIX: quantity now permit 0

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true
   validates :description, length: { in: 5..300 }, allow_blank: true
   validates :waiting_time, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
